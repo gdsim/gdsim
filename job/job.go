70537a21731cbc664cba334bf6f2d414eedad8ae
Daniel Alves <dalves@ucsc.edu>
2020-04-20 17:55:03 -0700

    Add job module.

---
+++ job/job.go
@@ -0,0 +1,56 @@
+package job
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type Task struct {
+	Duration uint64
+}
+
+type Job struct {
+	Submission uint64
+	Cpus       uint
+	Tasks      []*Task
+	User       string
+}
+
+func Load(reader io.Reader) ([]*Job, error) {
+	scanner := bufio.NewScanner(reader)
+	res := make([]*Job, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Split(line, " ")
+		if len(words) < 6 {
+			return nil, fmt.Errorf("failure to read job %d", len(res)+1)
+		}
+		j := &Job{
+			User:  words[3],
+			Tasks: make([]*Task, 0),
+		}
+		cpus, err := strconv.ParseUint(words[1], 0, 0)
+		if err != nil {
+			return nil, fmt.Errorf("failure to read job %d: %v", len(res)+1, err)
+		}
+		j.Cpus = uint(cpus)
+		j.Submission, err = strconv.ParseUint(words[1], 0, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failure to read job %d: %v", len(res)+1, err)
+		}
+		for i := 5; i < len(words); i++ {
+			d, err := strconv.ParseUint(words[i], 0, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failure to read job %d: %v", len(res)+1, err)
+			}
+			t := &Task{Duration: d}
+			j.Tasks = append(j.Tasks, t)
+		}
+		res = append(res, j)
+	}
+	return res, nil
+}
