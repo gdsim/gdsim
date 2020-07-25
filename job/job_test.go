718fd6ec2ccfa100302dad31940b0a5105e27df3
Daniel Alves <dalves@ucsc.edu>
2020-07-25 16:29:09 -0700

    Add tests to Job module.

---
+++ job/job_test.go
@@ -0,0 +1,43 @@
+package job
+
+import (
+	"strings"
+	"testing"
+)
+
+func validJob(t *testing.T, job Job, cpus uint, submission uint64, tasks []uint64, file string) bool {
+	if job.Submission != submission {
+		t.Errorf("expected job.Submission = %v, found %v", job.Submission, submission)
+	}
+	if job.Cpus != cpus {
+		t.Errorf("expected job.Cpus = %v, found %v", job.Cpus, cpus)
+	}
+	if len(job.Tasks) != len(tasks) {
+		t.Errorf("expected len(job.Tasks) = %v, found %v", len(tasks), len(job.Tasks))
+	}
+	for i, task := range job.Tasks {
+		if task.Duration != tasks[i] {
+			t.Errorf("expected job.Tasks[i].Duration = %v, found %v", tasks[i], task.Duration)
+		}
+	}
+	if job.File != file {
+		t.Errorf("expected job.File = %v, found %v", job.File, file)
+	}
+	return true
+}
+
+func TestLoad(t *testing.T) {
+	sample := "j1 1 0 f1 1 2\nj2 2 1 f2 7"
+	reader := strings.NewReader(sample)
+
+	jobs, err := Load(reader)
+	if err != nil {
+		t.Errorf("expected no error for sample '%v', found '%v'", sample, err)
+	}
+	numJobs := len(jobs)
+	if numJobs != 2 {
+		t.Errorf("expected len(jobs) = %v, found %v", 2, numJobs)
+	}
+	validJob(t, jobs[0], 1, 0, []uint64{1, 2}, "f1")
+	validJob(t, jobs[1], 2, 1, []uint64{7}, "f2")
+}
