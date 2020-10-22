f1f4744e6fd46cf068df51ce905682212ea109a5
Daniel Alves <dalves@ucsc.edu>
2020-10-21 21:21:34 -0700

    Add first version of scheduler module.
    This code has large amounts of commented code at the end, the purpose
    of this commit is to get an initial MVP running, then go back and
    clean up. The commented code is there to help in that direction.

---
+++ scheduler/scheduler.go
@@ -0,0 +1,177 @@
+package scheduler
+
+import (
+	"container/heap"
+	"github.com/dsfalves/simulator/file"
+	"github.com/dsfalves/simulator/job"
+	"github.com/dsfalves/simulator/scheduler/event"
+	"github.com/dsfalves/simulator/topology"
+	"sort"
+)
+
+type jobHeap []job.Job
+
+func rpt(j job.Job) uint64 {
+	var total uint64
+	for _, task := range j.Tasks {
+		total += task.Duration
+	}
+	return total
+}
+
+func (h jobHeap) Len() int           { return len(h) }
+func (h jobHeap) Less(i, j int) bool { return rpt(h[i]) < rpt(h[j]) }
+func (h jobHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *jobHeap) Push(x interface{}) {
+	*h = append(*h, x.(job.Job))
+}
+
+func (h *jobHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+type GlobalSRPTScheduler struct {
+	heap     jobHeap
+	topology topology.Topology
+}
+
+func NewGRPTS(t topology.Topology) GlobalSRPTScheduler {
+	scheduler := GlobalSRPTScheduler{
+		topology: t,
+	}
+	heap.Init(&scheduler.heap)
+	return scheduler
+}
+
+func (scheduler *GlobalSRPTScheduler) Add(j job.Job) {
+	sort.Slice(j.Tasks, func(i, k int) bool { return j.Tasks[i].Duration < j.Tasks[k].Duration })
+	heap.Push(&scheduler.heap, j)
+}
+
+func transferTime(size uint64, t topology.Topology, from, to int) uint64 {
+	if from == to {
+		return 0
+	}
+	return size / t.Speeds[from][to]
+}
+
+type transferCenter struct {
+	transferTime uint64
+	dataCenter   *topology.DataCenter
+}
+
+func bestDCs(f file.File, t topology.Topology) []transferCenter {
+	res := make([]transferCenter, len(t.DataCenters))
+
+	for i := range t.DataCenters {
+		res[i].dataCenter = t.DataCenters[i]
+		res[i].transferTime = transferTime(f.Size, t, f.Locations[0], i)
+		for k := 1; k < len(f.Locations); k++ {
+			from := f.Locations[k]
+			if transfer := transferTime(f.Size, t, from, i); transfer < res[i].transferTime {
+				res[i].transferTime = transfer
+			}
+		}
+	}
+	sort.Slice(res, func(i, k int) bool { return res[i].transferTime < res[k].transferTime })
+	return res
+}
+
+type taskEndEvent struct {
+	time uint64
+	cpus int
+	host *topology.Node
+}
+
+func (event taskEndEvent) Time() uint64 {
+	return event.time
+}
+
+func (event taskEndEvent) Process() []event.Event {
+	event.host.Free(event.cpus)
+	return nil
+}
+
+func (scheduler *GlobalSRPTScheduler) Schedule() []event.Event {
+	events := make([]event.Event, 0)
+	for scheduler.heap.Len() > 0 {
+		top := &scheduler.heap[0]
+		dcs := bestDCs(top.File, scheduler.topology)
+		for len(top.Tasks) > 0 {
+			hosted := false
+			for _, dc := range dcs {
+				if node, success := dc.dataCenter.Host(int(top.Cpus)); success {
+					task := top.Tasks[len(top.Tasks)-1]
+					top.Tasks = top.Tasks[:len(top.Tasks)-1]
+					events = append(events, taskEndEvent{
+						time: dc.transferTime + task.Duration,
+						cpus: int(top.Cpus),
+						host: node,
+					})
+					hosted = true
+					break
+				}
+			}
+			if !hosted {
+				return events
+			}
+			heap.Pop(&scheduler.heap)
+		}
+	}
+	return events
+}
+
+type JobScheduler interface {
+	//Pop() *job.Task
+	Add(t *job.Job)
+	Update() []event.Event
+}
+
+/*
+type jobEvent struct {
+	jobs []*job.Job
+	time uint64
+}
+
+func (je *jobEvent) Time() uint64 {
+	return je.time
+}
+
+func (je *jobEvent) Process(s *Scheduler) {
+	for _, job := range je.jobs {
+		s.js.Add(job)
+	}
+}
+
+type EventManager struct {
+	h  event.EventHeap
+	js JobScheduler
+}
+
+func New(jobs []*job.Job, js JobScheduler) *EventManager {
+	var s EventManager
+	s.js = js
+
+	return &s
+}
+
+func (s *EventManager) Run(jobs []*job.Job) {
+	var h EventHeap
+	for _, job := range jobs {
+		h.Push(job)
+	}
+	for len(h) > 0 {
+		next := h.Pop().(Event)
+		next.Process(s)
+		newEvents := s.js.Update()
+		for _, e := range newEvents {
+			h.Push(e)
+		}
+	}
+}
+*/
---
+++ scheduler/scheduler_test.go
@@ -0,0 +1,136 @@
+package scheduler
+
+import (
+	"github.com/dsfalves/simulator/file"
+	"github.com/dsfalves/simulator/job"
+	"github.com/dsfalves/simulator/topology"
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestGSRPT(t *testing.T) {
+	cap := [][2]int{
+		[2]int{1, 1},
+		[2]int{1, 1},
+	}
+	speeds := [][]uint64{
+		[]uint64{0, 10},
+		[]uint64{10, 0},
+	}
+	topo, err := topology.New(cap, speeds)
+	if err != nil {
+		t.Fatalf("failure to setup test: %v", err)
+	}
+	file1 := file.File{
+		Size:      100,
+		Locations: []int{0},
+	}
+	file2 := file.File{
+		Size:      200,
+		Locations: []int{1},
+	}
+	job1 := job.Job{
+		Id:         "job1",
+		Submission: 0,
+		Cpus:       1,
+		Tasks: []job.Task{
+			job.Task{100},
+		},
+		File: file1,
+	}
+	job2 := job.Job{
+		Id:         "job2",
+		Submission: 0,
+		Cpus:       1,
+		Tasks: []job.Task{
+			job.Task{20},
+		},
+		File: file2,
+	}
+
+	scheduler := NewGRPTS(*topo)
+
+	scheduler.Add(job1)
+	scheduler.Add(job2)
+
+	if len(scheduler.heap) != 2 {
+		t.Fatalf("error adding jobs, expected 2 added, found %v", len(scheduler.heap))
+	}
+	if !cmp.Equal(job2, scheduler.heap[0]) {
+		t.Errorf("error adding job, expected heap[0]=%v, found %v", job2, scheduler.heap[0])
+	}
+
+	events := scheduler.Schedule()
+	if len(events) != 2 {
+		t.Fatalf("error scheduling jobs, expected 2 scheduled, found %v", len(events))
+	}
+	if ev1 := events[0].(taskEndEvent); ev1.time != 20 {
+		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
+	}
+	if len(scheduler.heap) != 0 {
+		t.Fatalf("error scheduling jobs, expected job heap to have size 0, found %v", len(scheduler.heap))
+	}
+}
+
+/*
+type testJobScheduler struct {
+	time           uint64
+	procJobs       uint
+	procTasks      uint
+	remainingJobs  uint
+	remainingTasks uint
+}
+
+func (tjs *testJobScheduler) Time() uint64 {
+	return tjs.time
+}
+
+func TestRun(t *testing.T) {
+	jobs := []job.Job{
+		job.Job{
+			Id:         "j1",
+			Submission: 0,
+			Cpus:       1,
+			Tasks: []job.Task{
+				job.Task{1},
+				job.Task{2},
+			},
+			File: "f1",
+		},
+		job.Job{
+			Id:         "j2",
+			Submission: 0,
+			Cpus:       1,
+			Tasks: []job.Task{
+				job.Task{1},
+				job.Task{2},
+			},
+			File: "f2",
+		},
+	}
+	topo, err := topology.New(
+		[][2]int{
+			[2]int{1, 2},
+		},
+		[][]int{
+			[]int{1000},
+		},
+	)
+	if err != nil {
+		t.Fatalf("error when creating topology: %v", err)
+	}
+	files := map[string]file.File{
+		"f1": file.File{
+			Size:      10,
+			Locations: []*topology.DataCenter{topo.DataCenters[0]},
+		},
+		"f2": file.File{
+			Size:      10,
+			Locations: []*topology.DataCenter{topo.DataCenters[0]},
+		},
+	}
+	var tjs testJobScheduler
+
+	tjs.time = 0
+}
+*/
