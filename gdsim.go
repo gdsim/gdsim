5b8aa42ff1d88938555f546fd5d4f103701221c5
Daniel Alves <dalves@ucsc.edu>
2020-10-29 20:43:16 -0700

    Change output to bring it in line with existing tools.

--- gdsim.go
+++ gdsim.go
@@ -11,6 +11,8 @@ import (
 	"github.com/dsfalves/gdsim/scheduler"
 	"github.com/dsfalves/gdsim/simulator"
 	"github.com/dsfalves/gdsim/topology"
+	"io/ioutil"
+	"strings"
 )
 
 func check(err error) {
@@ -46,33 +48,44 @@ func printResults(results map[string]*job.Job) {
 
 	for id, j := range results {
 		tasks := make([]string, len(j.Scheduled))
-		for _, task := range j.Scheduled {
-			tasks = append(tasks, fmt.Sprintf("(%s, %s, %v, %v, %v)", j.File.Id, task.Location, j.Submission, task.Start, task.Start+task.Duration))
+		for i, task := range j.Scheduled {
+			tasks[i] = fmt.Sprintf("('%s', '%s', %v, %v, %v)", j.File.Id, task.Location, j.Submission, task.Start, task.Start+task.Duration)
 		}
-		fmt.Printf("%s %v %v\n", id, j.Submission, tasks)
+		fmt.Printf("%s %v [%v]\n", id, j.Submission, strings.Join(tasks, ", "))
 	}
 }
 
+func printFiles(files map[string]file.File) {
+	fmt.Print("{")
+	for key, value := range files {
+		locations := make([]string, len(value.Locations))
+		for i, loc := range value.Locations {
+			locations[i] = fmt.Sprintf("'DC%v'", loc)
+		}
+		fmt.Printf("'%s': (%v, [%s])", key, value.Size, strings.Join(locations, ", "))
+	}
+	fmt.Println("}")
+}
+
 func main() {
-	schedulerPtr := flag.String("scheduler", "SRPT", "type of scheduler to be used")
+	//schedulerPtr := flag.String("scheduler", "SRPT", "type of scheduler to be used")
 	topologyPtr := flag.String("topology", "topology.dat", "topology description file")
 	filesPtr := flag.String("files", "files.dat", "files description file")
 	window := flag.Float64("window", 3, "scheduling window size")
 	flag.Parse()
-	fmt.Println(*topologyPtr)
-	fmt.Println(*filesPtr)
-	fmt.Println(flag.Args())
 	if len(flag.Args()) < 1 {
 		log.Fatal("missing files to run")
 	}
+	log.SetFlags(0)
+	log.SetOutput(ioutil.Discard)
 
 	topo, err := loadTopology(*topologyPtr)
 	check(err)
 	files, err := loadFiles(*filesPtr)
 	check(err)
+	printFiles(files)
 
 	filename := flag.Args()[0]
-	fmt.Println(filename, *schedulerPtr)
 	jobs, err := loadJobs(filename, files)
 	check(err)
 	scheduler := scheduler.NewGRPTS(*topo)
