19a7d781dbc0aa15340bb0f1e8ab7e32f10f38fe
Daniel Alves <dalves@ucsc.edu>
2020-11-13 00:29:45 -0800

    Move SRPT scheduler to separate file.

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,8 +1,6 @@
 package scheduler
 
 import (
-	"container/heap"
-	"fmt"
 	"github.com/dsfalves/gdsim/file"
 	"github.com/dsfalves/gdsim/job"
 	"github.com/dsfalves/gdsim/scheduler/event"
@@ -13,6 +11,8 @@ import (
 
 type jobHeap []*job.Job
 
+// TODO: I shouldn't be calling rpt all the time
+// Make it so the first time calculates, then it marks the job as clean/dirty
 func rpt(j job.Job) uint64 {
 	var total uint64
 	for _, task := range j.Tasks {
@@ -37,27 +37,6 @@ func (h *jobHeap) Pop() interface{} {
 	return x
 }
 
-type GlobalSRPTScheduler struct {
-	heap     jobHeap
-	topology topology.Topology
-	jobs     map[string]*job.Job
-}
-
-func NewGRPTS(t topology.Topology) GlobalSRPTScheduler {
-	scheduler := GlobalSRPTScheduler{
-		topology: t,
-		jobs:     make(map[string]*job.Job),
-	}
-	heap.Init(&scheduler.heap)
-	return scheduler
-}
-
-func (scheduler *GlobalSRPTScheduler) Add(j *job.Job) {
-	sort.Slice(j.Tasks, func(i, k int) bool { return j.Tasks[i].Duration < j.Tasks[k].Duration })
-	heap.Push(&scheduler.heap, j)
-	scheduler.jobs[j.Id] = j
-}
-
 func transferTime(size uint64, t topology.Topology, from, to int) uint64 {
 	if from == to {
 		return 0
@@ -65,13 +44,10 @@ func transferTime(size uint64, t topology.Topology, from, to int) uint64 {
 	return size / t.Speeds[from][to]
 }
 
-func (scheduler GlobalSRPTScheduler) Results() map[string]*job.Job {
-	return scheduler.jobs
-}
-
 type transferCenter struct {
-	transferTime uint64
-	dataCenter   *topology.DataCenter
+	transferTime           uint64
+	freeJobSlots, capacity int
+	dataCenter             *topology.DataCenter
 }
 
 func bestDCs(f file.File, t topology.Topology) []transferCenter {
@@ -94,17 +70,19 @@ func bestDCs(f file.File, t topology.Topology) []transferCenter {
 type taskEndEvent struct {
 	start, duration uint64
 	cpus            int
-	host            *topology.Node
 	where           string
 	job             *job.Job
 }
 
-func (event taskEndEvent) Time() uint64 {
+func (event taskEndEvent) End() uint64 {
 	return event.start + event.duration
 }
 
+func (event taskEndEvent) Cpus() int {
+	return event.cpus
+}
+
 func (event taskEndEvent) Process() []event.Event {
-	event.host.Free(event.cpus)
 	event.job.Scheduled = append(event.job.Scheduled, job.DoneTask{
 		Start:    event.start,
 		Duration: event.duration,
@@ -112,39 +90,6 @@ func (event taskEndEvent) Process() []event.Event {
 	})
 	log.Printf("added event to Scheduled - len(Scheduled) = %v\n", len(event.job.Scheduled))
 	return nil
-}
-
-func (scheduler *GlobalSRPTScheduler) Schedule(now uint64) []event.Event {
-	events := make([]event.Event, 0)
-	for scheduler.heap.Len() > 0 {
-		top := scheduler.heap[0]
-		dcs := bestDCs(top.File, scheduler.topology)
-		for len(top.Tasks) > 0 {
-			hosted := false
-			for i, dc := range dcs {
-				if node, success := dc.dataCenter.Host(int(top.Cpus)); success {
-					task := top.Tasks[len(top.Tasks)-1]
-					top.Tasks = top.Tasks[:len(top.Tasks)-1]
-					events = append(events, taskEndEvent{
-						start:    dc.transferTime + now,
-						duration: task.Duration,
-						cpus:     int(top.Cpus),
-						host:     node,
-						where:    fmt.Sprintf("DC%v", i),
-						job:      top,
-					})
-					hosted = true
-					log.Printf("scheduling task %v for job %v\n", task, top.Id)
-					break
-				}
-			}
-			if !hosted {
-				return events
-			}
-		}
-		heap.Pop(&scheduler.heap)
-	}
-	return events
 }
 
 type Scheduler interface {
---
+++ scheduler/srpt.go
@@ -0,0 +1,72 @@
+package scheduler
+
+import (
+	"container/heap"
+	"fmt"
+	"github.com/dsfalves/gdsim/job"
+	"github.com/dsfalves/gdsim/scheduler/event"
+	"github.com/dsfalves/gdsim/topology"
+	"log"
+	"sort"
+)
+
+type GlobalSRPTScheduler struct {
+	heap     jobHeap
+	topology topology.Topology
+	jobs     map[string]*job.Job
+}
+
+func NewGRPTS(t topology.Topology) GlobalSRPTScheduler {
+	scheduler := GlobalSRPTScheduler{
+		topology: t,
+		jobs:     make(map[string]*job.Job),
+	}
+	heap.Init(&scheduler.heap)
+	return scheduler
+}
+
+func (scheduler *GlobalSRPTScheduler) Add(j *job.Job) {
+	sort.Slice(j.Tasks, func(i, k int) bool { return j.Tasks[i].Duration < j.Tasks[k].Duration })
+	heap.Push(&scheduler.heap, j)
+	scheduler.jobs[j.Id] = j
+}
+
+func (scheduler *GlobalSRPTScheduler) Schedule(now uint64) []event.Event {
+	events := make([]event.Event, 0)
+	for scheduler.heap.Len() > 0 {
+		top := scheduler.heap[0]
+		dcs := bestDCs(top.File, scheduler.topology)
+		for len(top.Tasks) > 0 {
+			hosted := false
+			for i, dc := range dcs {
+				task := top.Tasks[len(top.Tasks)-1]
+				taskEnd := taskEndEvent{
+					start:    dc.transferTime + now,
+					duration: task.Duration,
+					cpus:     int(top.Cpus),
+					where:    fmt.Sprintf("DC%v", i),
+					job:      top,
+				}
+				if node, success := dc.dataCenter.Host(taskEnd); success {
+					top.Tasks = top.Tasks[:len(top.Tasks)-1]
+					taskEnd.Process()
+					hosted = true
+					log.Printf("scheduling task %v for job %v\n", task, top.Id)
+					if node.QueueLen() == 1 {
+						events = append(events, node)
+					}
+					break
+				}
+			}
+			if !hosted {
+				return events
+			}
+		}
+		heap.Pop(&scheduler.heap)
+	}
+	return events
+}
+
+func (scheduler GlobalSRPTScheduler) Results() map[string]*job.Job {
+	return scheduler.jobs
+}
