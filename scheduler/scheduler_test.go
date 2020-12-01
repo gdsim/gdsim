b2fdc7f18e7543cdee6b506c6aa308a6724ea5e3
Daniel Alves <dalves@ucsc.edu>
2020-11-30 21:43:00 -0800

    Refactor tests to minimize code duplication.

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -1,12 +1,49 @@
 package scheduler
 
 import (
+	"container/heap"
 	"github.com/dsfalves/gdsim/file"
 	"github.com/dsfalves/gdsim/job"
+	"github.com/dsfalves/gdsim/scheduler/event"
 	"github.com/dsfalves/gdsim/topology"
 	"github.com/google/go-cmp/cmp"
 	"testing"
 )
+
+type schedulerHeap interface {
+	Top() *job.Job
+	heap.Interface
+}
+
+func checkHeap(t *testing.T, heap schedulerHeap, length int, j *job.Job) {
+	if heap.Len() != 2 {
+		t.Fatalf("error adding jobs, expected %d added, found %v", length, heap.Len())
+	}
+	top := heap.Top()
+	if !cmp.Equal(*j, *top) {
+		t.Errorf("error adding job, expected heap[0]=%v, found %v", j, top)
+	}
+}
+
+type expected struct {
+	time uint64
+	node *topology.Node
+}
+
+func checkEvents(t *testing.T, events []event.Event, answers []expected) {
+	if len(events) != len(answers) {
+		t.Fatalf("error scheduling jobs, expected %v scheduled, found %v", len(answers), len(events))
+	}
+	for i, e := range answers {
+		ev, ok := events[i].(*topology.Node)
+		if !ok {
+			t.Fatalf("error in return of Schedule: %v is not *topology.Node", ev)
+		}
+		if time := ev.Time(); time != e.time {
+			t.Errorf("error scheduling jobs, expected task ending at %d, found %d", e.time, time)
+		}
+	}
+}
 
 func TestGSRPT(t *testing.T) {
 	cap := [][2]int{
@@ -53,31 +90,20 @@ func TestGSRPT(t *testing.T) {
 	scheduler.Add(&job1)
 	scheduler.Add(&job2)
 
-	if len(scheduler.heap) != 2 {
-		t.Fatalf("error adding jobs, expected 2 added, found %v", len(scheduler.heap))
-	}
-	if !cmp.Equal(job2, *scheduler.heap[0]) {
-		t.Errorf("error adding job, expected heap[0]=%v, found %v", job2, scheduler.heap[0])
-	}
+	checkHeap(t, &scheduler.heap, 2, &job2)
 
 	events := scheduler.Schedule(0)
-	if len(events) != 2 {
-		t.Fatalf("error scheduling jobs, expected 2 scheduled, found %v", len(events))
+	answers := []expected{
+		expected{
+			time: 20,
+			node: topo.DataCenters[1].Get(0),
+		},
+		expected{
+			time: 100,
+			node: topo.DataCenters[0].Get(0),
+		},
 	}
-	ev1 := events[0].(*topology.Node)
-	if ev1.Time() != 20 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
-	}
-	if node := topo.DataCenters[1].Get(0); ev1 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev1)
-	}
-	ev2 := events[1].(*topology.Node)
-	if ev2.Time() != 100 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
-	}
-	if node := topo.DataCenters[0].Get(0); ev2 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev2)
-	}
+	checkEvents(t, events, answers)
 	if len(scheduler.heap) != 0 {
 		t.Fatalf("error scheduling jobs, expected job heap to have size 0, found %v", len(scheduler.heap))
 	}
@@ -129,31 +155,20 @@ func TestGSRPT2(t *testing.T) {
 	scheduler.Add(&job1)
 	scheduler.Add(&job2)
 
-	if len(scheduler.heap) != 2 {
-		t.Fatalf("error adding jobs, expected 2 added, found %v", len(scheduler.heap))
-	}
-	if !cmp.Equal(job1, *scheduler.heap[0]) {
-		t.Errorf("error adding job, expected heap[0]=%v, found %v", job1, scheduler.heap[0])
-	}
+	checkHeap(t, &scheduler.heap, 2, &job1)
 
 	events := scheduler.Schedule(0)
-	if len(events) != 2 {
-		t.Fatalf("error scheduling jobs, expected 2 scheduled, found %v", len(events))
+	answers := []expected{
+		expected{
+			time: 30,
+			node: topo.DataCenters[0].Get(0),
+		},
+		expected{
+			time: 20,
+			node: topo.DataCenters[1].Get(0),
+		},
 	}
-	ev1 := events[0].(*topology.Node)
-	if ev1.Time() != 30 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
-	}
-	if node := topo.DataCenters[0].Get(0); ev1 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev1)
-	}
-	ev2 := events[1].(*topology.Node)
-	if ev2.Time() != 20 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
-	}
-	if node := topo.DataCenters[1].Get(0); ev2 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev2)
-	}
+	checkEvents(t, events, answers)
 	if len(scheduler.heap) != 1 {
 		t.Fatalf("error scheduling jobs, expected job heap to have size 0, found %v", len(scheduler.heap))
 	}
@@ -204,32 +219,21 @@ func TestGeoDis(t *testing.T) {
 	scheduler.Add(&job1)
 	scheduler.Add(&job2)
 
-	if scheduler.heap.Len() != 2 {
-		t.Fatalf("error adding jobs, expected 2 added, found %v", scheduler.heap.Len())
-	}
 	scheduler.Update(0)
-	if !cmp.Equal(job2, scheduler.heap.jobPile[0].Job) {
-		t.Errorf("error adding job, expected heap[0]=%v, found %v", job2, scheduler.heap.jobPile[0])
-	}
+	checkHeap(t, &scheduler.heap, 2, &job2)
 
 	events := scheduler.Schedule(0)
-	if len(events) != 2 {
-		t.Fatalf("error scheduling jobs, expected 2 scheduled, found %v", len(events))
+	answers := []expected{
+		expected{
+			time: 20,
+			node: topo.DataCenters[1].Get(0),
+		},
+		expected{
+			time: 100,
+			node: topo.DataCenters[0].Get(0),
+		},
 	}
-	ev1 := events[0].(*topology.Node)
-	if ev1.Time() != 20 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
-	}
-	if node := topo.DataCenters[1].Get(0); ev1 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev1)
-	}
-	ev2 := events[1].(*topology.Node)
-	if ev2.Time() != 100 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v", ev1)
-	}
-	if node := topo.DataCenters[0].Get(0); ev2 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev2)
-	}
+	checkEvents(t, events, answers)
 	if scheduler.heap.Len() != 0 {
 		t.Fatalf("error scheduling jobs, expected job heap to have size 0, found %v", scheduler.heap.Len())
 	}
@@ -281,32 +285,22 @@ func TestGeoDis2(t *testing.T) {
 	scheduler.Add(&job1)
 	scheduler.Add(&job2)
 
-	if scheduler.heap.Len() != 2 {
-		t.Fatalf("error adding jobs, expected 2 added, found %v", scheduler.heap.Len())
-	}
 	scheduler.Update(0)
-	if !cmp.Equal(job2, scheduler.heap.jobPile[0].Job) {
-		t.Errorf("error adding job, expected heap[0]=%v, found %v", job2, scheduler.heap.jobPile[0])
-	}
+	checkHeap(t, &scheduler.heap, 2, &job2)
 
 	events := scheduler.Schedule(0)
-	if len(events) != 2 {
-		t.Fatalf("error scheduling jobs, expected 2 scheduled, found %v", len(events))
+	answers := []expected{
+		expected{
+			time: 20,
+			node: topo.DataCenters[1].Get(0),
+		},
+		expected{
+			time: 21,
+			node: topo.DataCenters[0].Get(0),
+		},
 	}
-	ev1 := events[0].(*topology.Node)
-	if time := ev1.Time(); time != 20 {
-		t.Errorf("error scheduling jobs, expected task ending at 20, found %v for %v", time, ev1)
-	}
-	if node := topo.DataCenters[1].Get(0); ev1 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev1)
-	}
-	ev2 := events[1].(*topology.Node)
-	if time := ev2.Time(); time != 21 { // 1s for transferring file
-		t.Errorf("error scheduling jobs, expected task ending at 30, found %v for %v", time, ev2)
-	}
-	if node := topo.DataCenters[0].Get(0); ev2 != node {
-		t.Errorf("error scheduling jobs, expected task in node %v, found at %v", node, ev2)
-	}
+	checkEvents(t, events, answers)
+
 	if scheduler.heap.Len() != 1 {
 		t.Fatalf("error scheduling jobs, expected job heap to have size 0, found %v", scheduler.heap.Len())
 	}
